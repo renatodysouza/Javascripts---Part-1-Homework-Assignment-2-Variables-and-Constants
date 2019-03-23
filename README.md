07dc66658cd775f72a11827c49229731fd42932c
Renato <Renato>
2019-03-23 12:48:28 +0000

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+## Homework Assignment #2: Variables and Constants
+
+
+Details:
+ 
+What are the differences between let, const and var? When would each be appropriate too use? In your own words, write 1 - 2 paragraphs explaining the different use-cases for each. If English is not your native language, feel free to write in whatever language you prefer. Just please note which language it is at the top of the assignment, so we know how to translate it.
+
+Create a file called variables.js and add your explanation as comments at the top of the page.
+
+Then, within the document write 3 code examples (1 for var, 1 for const and 1 for let) showcasing the use-cases you explained above.
---
+++ variables.js
@@ -0,0 +1,61 @@
+/**
+ * 
+ * 
+ *  Diferences between var, let and const
+ * 
+ */
+
+
+/**
+ * VAR
+ * The diference is about the scope, for example: var, the global scope. When use var the variable is acessible globaly
+ */
+
+var cars = 'Fiat';
+function agencia() {
+    cars = 'ford'
+    return cars;
+}
+
+
+console.log(agencia());
+console.log(cars);
+
+
+/**
+ * Let
+ * 
+ * The let is limit in the scope, 
+ */
+
+
+var cars2 = 'Fiat';
+function agencia2() {
+    let cars2 = 'ford'
+    return cars2;
+}
+
+
+console.log(agencia2());
+console.log(cars2);
+
+
+/**
+* CONST    
+* 
+* The const is limit in the scope, and it not permit change the value of the variable, but permit change value of the object
+*/
+
+const data = '2018';
+data = '2020';
+console.log(data); // TypeError: Assignment to constant variable.
+
+
+// In object
+
+const datas = { 'd1': '2018', 'd2': '2019' }
+
+datas.d1 = '2020';
+
+console.log(datas);
+
